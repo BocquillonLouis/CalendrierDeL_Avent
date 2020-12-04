d20fbe4dd7123e19126e9f9b6e9ef308685fc5a2
BocquillonLouis <louis.bocquillonliger-belair@iut-rodez.fr>
2020-12-04 04:35:39 +0100

    Corrections énigme 1

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     require "src/html/gestionSession.php";
 
+    $_SESSION['masque'] = 0;
+
     if (!isset($_SESSION['masque']) || isset($_SESSION['masque']) && $_SESSION['masque'] == 0) {
         // Rien car on est déjà sur l'index
     } else if ($_SESSION['masque'] != 0) {
@@ -21,18 +23,29 @@
         <link href="src/bootstrap/bootstrap.css" rel="stylesheet">
 
         <!-- CSS -->
-        <link href="src/css/index.css" rel="stylesheet">
+        <link href="src/css/4.css" rel="stylesheet">
     </head>
 
     <body>
-        <?php
-            $_SESSION['masque'] = 1;
-        ?>
-        <center>
-            Cliquez sur le bouton pour obtenir le premier masque.<br><br><br><br><br><br><br><br><br><br><br><br><br>
-        
-            <button><a href="src/html/2.php">Cliquez ici</a></button>
-        </center>
-        
+        <div class="container d-flex h-100 flex-column">
+
+            <div class="row my-auto text-center" id="part1">
+                <div class="col-md-12">
+
+                    <?php if (isset($_GET['masqueTrouve']) && $_GET['masqueTrouve'] == "true") {
+                        $_SESSION['masque'] = 1;
+                    ?>
+                        <h1 class="trouve">Félicitations, tu peux passer à l'énigme suivante !</h1>
+                        <br /><img src="src/img/1.png"><br /><br />
+                        <a href="src/html/2.php" class="btn btn-primary trouve">Énigme suivante</a>
+                    <?php } else { ?>
+                        <p>Cliquez sur ce bouton pour obtenir le premier masque.</p>
+                        <a class="btn btn-primary" href="index.php?masqueTrouve=true">Clique !</a>
+                    <?php } ?>
+                    
+                </div>
+            </div>
+        </div>
     </body>
+
 </html>
