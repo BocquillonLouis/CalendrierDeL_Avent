99abd3e5712d29b43a5b06855b2f3569c7c72b68
BocquillonLouis <louis.bocquillonliger-belair@iut-rodez.fr>
2020-12-04 04:12:54 +0100

    Ajout énigme 3

--- src/html/3.php
+++ src/html/3.php
@@ -21,25 +21,44 @@
         <link href="../bootstrap/bootstrap.css" rel="stylesheet">
 
         <!-- CSS -->
-        <link href="../css/2.css" rel="stylesheet">
+        <link href="../css/4.css" rel="stylesheet">
     </head>
 
     <body>
 
         <div class="container d-flex h-100 flex-column">
 
-            <div class="row my-auto text-center" id="part1">
+            <div class="my-auto text-center">
                 <div class="col-md-12">
 
                     <?php if (isset($_GET['masqueTrouve']) && $_GET['masqueTrouve'] == "true") {
                         $_SESSION['masque'] = 3;
                     ?>
-                        <!-- Code si masque trouvé (énigme résolue) -->
+                        <h1 class="trouve">Félicitations, tu peux passer à l'énigme suivante !</h1>
+                        <br /><img src="../img/3.png"><br /><br />
+                        <a href="4.php" class="btn btn-primary trouve">Énigme suivante</a>
 
                     <?php } else { ?>
-                        <!-- Code si masque pas encore trouvé -->
-                        <?php echo 'session : ' . $_SESSION['masque']; ?>
-                        
+                        <?php
+                        echo '<p class="lead">';
+                            for ($j=0; $j < 32; $j++) { 
+                                for ($i = 0; $i < 100; $i++) { 
+                                    echo 'o';
+                                }
+                                echo "<br />";
+                            }
+                            
+                            echo 'oooooooooooooooooooooo<a href="3.php?masqueTrouve=true">0</a>ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo<br />';
+
+                            for ($j=0; $j < 26; $j++) { 
+                                for ($i = 0; $i < 100; $i++) { 
+                                    echo 'o';
+                                }
+                                echo "<br />";
+                            }
+                            echo '</p>';
+                        ?>
+
                     <?php } ?>
                     
                 </div>
